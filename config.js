5ec0a481da954c5195b4c8c2984b62dcc713d9b6
Tin74Tin <tintecksiong@gmail.com>
2026-08-04 21:32:41 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase project connection settings for Flex Benefits Portal.
+window.SUPABASE_CONFIG = {
+  url: 'https://chibgftcqblzgpcpdeiz.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoaWJnZnRjcWJsemdwY3BkZWl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NDYwMzIsImV4cCI6MjEwMTQyMjAzMn0.ndYez8_lnjWOoYislMJ3xGEZ1JK4l3gmN8v5-MYTx-0'
+};
